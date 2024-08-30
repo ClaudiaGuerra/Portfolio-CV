695810beb337f7a2647c1e00291e8af9e5ec6e65
Claudia Guerra <claudiagbak@gmail.com>
2024-08-30 13:50:57 +0100

    Contact form updated

---
+++ email.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'ClaudiaGuerraBakWebsite';
+   const templateID = 'template_n6b4stn';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
